1978f423b94392c2fd315cbac2955c617e4130d2
Vansh Gupta <vansh@veersatech.com>
2026-07-11 18:53:58 +0530

    basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Hello } from './hello/hello';
+import { TodoList } from './todo-list/todo-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"hello",component:Hello},
+    {path:"todo",component:TodoList}
+];
